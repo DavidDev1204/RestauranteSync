d954fbf7fb43d923a4806c23b832298d710a04b0
DavidDev1204 <104733936+DavidDev1204@users.noreply.github.com>
2025-09-06 11:54:57 -0500

    Update Program.cs

--- RestauranteSync.API/Program.cs
+++ RestauranteSync.API/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.WebHost.UseUrls("http://+:5000");
+
 // Configure Swagger
 builder.Services.AddSwaggerGen(options =>
 {
